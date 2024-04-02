810ad9934b6f8008486277a42932f01f376f14b7
joaovitorlopes <jvlaaa+github@gmail.com>
2024-04-01 23:22:10 -0400

    adding Main componets

--- tasks-list/src/App.js
+++ tasks-list/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import Main from './components/Main';
+
 import './App.css';
 
 export default function App() {
-  return (
-    <>
-      <h1>Hello world!</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </>
-  );
+  return <Main />;
 }
